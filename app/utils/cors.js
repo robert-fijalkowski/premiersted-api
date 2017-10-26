d59ad8eab256156fc37baff70bf5746e2dd15b1e
Mateusz Odelga <mateusz.odelga@schibsted.pl>
2017-10-26 12:56:10 +0200

    Support cors on any localhost port

--- app/utils/cors.js
+++ app/utils/cors.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 
 module.exports = (app) => {
   app.use(cors({
-    origin: ['http://localhost:7000', /http(s)?:\/\/([^.]+\.)*premiersted.schibsted.ga$/],
+    origin: [/^http(s)?:\/\/localhost/, /http(s)?:\/\/([^.]+\.)*premiersted.schibsted.ga$/],
     allowedHeaders: ['auth-token'],
     preflightContinue: false,
   }));
