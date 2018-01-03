e7621a9ec1b8c15c75b59a69776720f8a06861ec
Mateusz Odelga <mateusz.odelga@schibsted.pl>
2018-01-03 01:14:44 +0100

    Add possibility to set-up parent as existing game.

--- app/services/games/complete/applyChanges.js
+++ app/services/games/complete/applyChanges.js
@@ -5,10 +5,23 @@ const { parseChanges } = require('./operations');
 const detailedGame = require('../detailedGame');
 
 const joinMsg = 'User api://users/{{competitor.uid}} joined to api://games/{{game.id}} with club api://clubs/{{competitor.club}}';
-const create = parent => async ({ item: { name, location, competitors } }) => {
-  const game = await db.games.create({ name, location, parent: parent.id });
+
+const tryToReuseGame = async (parent, nameOrId, location) => {
+  const availableGame = await db.games.findById(nameOrId);
+  if (availableGame && availableGame.status === 'OPEN') {
+    await db.games.update({
+      ...availableGame,
+      parents: R.uniq((availableGame.parents || []).concat(parent.id)),
+    });
+    return availableGame;
+  }
+  return db.games.create({ name: nameOrId, location, parents: [parent.id] });
+};
+
+const scheduleContinuation = parent => async ({ item: { name, location, competitors } }) => {
+  const game = await tryToReuseGame(parent, name, location);
   const events = [systemEvent({ type: 'games' }), systemEvent({ type: 'games', relate: game.id })];
-  events.forEach(e => e('api://games/{{id}} was created as child of api://games/{{parent}}', game));
+  events.forEach(e => e('api://games/{{id}} was pointed as child of api://games/{{parent}}', game));
   await Promise.all(competitors.map((competitor) => {
     const joinEvent = systemEvent({ type: 'users', relate: competitor.uid });
     return db.competitors.add({ gid: game.id, ...competitor })
@@ -18,10 +31,10 @@ const create = parent => async ({ item: { name, location, competitors } }) => {
 };
 
 const complete = async (toComplete, continueIn) => {
-  const parent = toComplete.item;
-  const parentGame = await db.games.findById(parent.id);
+  const completingGame = toComplete.item;
+  const details = await db.games.findById(completingGame.id);
   return db.games.update({
-    ...parentGame,
+    ...details,
     status: 'COMPLETED',
     continueIn,
   });
@@ -30,7 +43,7 @@ const complete = async (toComplete, continueIn) => {
 module.exports = async (changes) => {
   const { toComplete, toCreate } = parseChanges(changes);
   const parent = toComplete.item;
-  const created = await Promise.all(toCreate.map(create(parent)));
+  const created = await Promise.all(toCreate.map(scheduleContinuation(parent)));
   await complete(toComplete, created.reduce(R.merge, {}));
   return detailedGame({ gid: parent.id });
 };
